143be1be821d7e6139e4a21609253f616107ef1c
Venkat Kurapati <venkat@giffgaffmvenkat.local>
2017-01-11 11:22:19 +0000

    Implemented main  file as per requirement

--- Problem Set/GuitarString/GuitarString/main.m
+++ Problem Set/GuitarString/GuitarString/main.m
@@ -13,10 +13,16 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         // a. Create an instance of the class GuitarString
+        GuitarString *guitarString = [[GuitarString alloc]init];
         // b. Create an NSError
+        NSError *error;
         // c. Call the method pluck(velocity: Float)
+        [guitarString pluck:0.9 error:&error];
         // d. Check if an error was returned
         // e. Log an error if one was returned
+        if (error) {
+            NSLog(@"Error occured with domain: %@ and code %ld", error.domain, (long)error.code);
+        }
 
     }
     return 0;
